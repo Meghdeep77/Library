fcfc1b36531a0f00af13697f1264037f4f2e93d9
Meghdeep77 <117975443+Meghdeep77@users.noreply.github.com>
2024-01-19 15:23:37 +0530

    4 points + RTL

--- final.py
+++ final.py
@@ -1,23 +1,23 @@
 from __future__ import print_function
 import time
 from dronekit import connect, VehicleMode, LocationGlobalRelative
+from geopy.distance import geodesic
 
 
-# Set up option parsing to get connection string
 
 
 connection_string = 'udp:127.0.0.1:14551'
 sitl = None
 
 
-# Start SITL if no connection string specified
+
 if not connection_string:
     import dronekit_sitl
     sitl = dronekit_sitl.start_default()
     connection_string = sitl.connection_string()
 
 
-# Connect to the Vehicle
+
 print('Connecting to vehicle on: %s' % connection_string)
 vehicle = connect(connection_string, wait_ready=True)
 
@@ -58,41 +58,101 @@ def print_current_coordinates():
     print("Current Coordinates: Lat={}, Lon={}, Alt={}".format(
         current_location.lat, current_location.lon, current_location.alt
     ))
+def get_distance_metres(location1, location2):
+    coords1 = (location1.lat, location1.lon)
+    coords2 = (location2.lat, location2.lon)
+    return geodesic(coords1, coords2).meters
 
 #vehicle.wait_ready('home_location', timeout=10)
-current_location = vehicle.location.global_relative_frame
+launch_location = vehicle.location.global_relative_frame
 
-launch_latitude = current_location.lat
-launch_longitude = current_location.lon
-launch_altitude = current_location.alt
+launch_latitude = launch_location.lat
+launch_longitude = launch_location.lon
+launch_altitude = launch_location.alt
 launch  = LocationGlobalRelative(launch_latitude, launch_longitude , launch_altitude)
+print("Launch Coordinates: Lat={}, Lon={}, Alt={}".format(
+        launch_latitude, launch_longitude, launch_altitude
+    ))
 arm_and_takeoff(10)
 print_current_coordinates()
 print("Set default/target airspeed to 3")
 vehicle.airspeed = 10
 
-print("Going towards first point for 30 seconds ...")
+print("Going towards first point ...")
 point1 = LocationGlobalRelative(-35.361354, 149.165218, 20)
 vehicle.simple_goto(point1)
 
 
+while True:
+    print_current_coordinates()
+    current_location = vehicle.location.global_relative_frame
+    distance_to_target = get_distance_metres(current_location, point1)
+    print("Distance to target: ", distance_to_target)
+    if distance_to_target < 1:
+        print("Reached the first point.")
+        break
+    time.sleep(1)
 
-time.sleep(30)
-print_current_coordinates()
-print("Going towards second point for 30 seconds (groundspeed set to 10 m/s) ...")
+
+print("Going towards second point (groundspeed set to 20 m/s) ...")
 point2 = LocationGlobalRelative(-35.363244, 149.168801, 20)
 vehicle.simple_goto(point2, groundspeed=20)
 
 
-time.sleep(30)
-print_current_coordinates()
+while True:
+    print_current_coordinates()
+    current_location = vehicle.location.global_relative_frame
+    distance_to_target = get_distance_metres(current_location, point2)
+    print("Distance to target: ", distance_to_target)
+    if distance_to_target < 1:
+        print("Reached the second point.")
+        break
+    time.sleep(1)
+
+print("Going towards Third point (groundspeed set to 20 m/s) ...")
+point3 = LocationGlobalRelative(-35.364314, 149.17022258, 20)
+vehicle.simple_goto(point3, groundspeed=20)
+
+# Wait until the drone reaches the second point
+while True:
+    print_current_coordinates()
+    current_location = vehicle.location.global_relative_frame
+    distance_to_target = get_distance_metres(current_location, point3)
+    print("Distance to target: ", distance_to_target)
+    if distance_to_target < 1:
+        print("Reached the third point.")
+        break
+    time.sleep(1)
+
+print("Going towards Fourth point (groundspeed set to 20 m/s) ...")
+point4 = LocationGlobalRelative(-35.368394, 149.172178, 20)
+vehicle.simple_goto(point4, groundspeed=20)
+
+# Wait until the drone reaches the second point
+while True:
+    print_current_coordinates()
+    current_location = vehicle.location.global_relative_frame
+    distance_to_target = get_distance_metres(current_location, point4)
+    print("Distance to target: ", distance_to_target)
+    if distance_to_target < 1:
+        print("Reached the Fourth point.")
+        break
+    time.sleep(1)
 # Return to Launch (RTL)
 print("Returning to Launch")
 print("Launch Coordinates: Lat={}, Lon={}, Alt={}".format(
         launch_latitude, launch_longitude, launch_altitude
     ))
 vehicle.simple_goto(launch, groundspeed=20)
-time.sleep(30)
+while True:
+    print_current_coordinates()
+    current_location = vehicle.location.global_relative_frame
+    distance_to_target = get_distance_metres(current_location, launch)
+    print("Distance to target: ", distance_to_target)
+    if distance_to_target < 1:
+        print("Reached the launch point.")
+        break
+    time.sleep(1)
 while vehicle.location.global_relative_frame.alt > 0:
     print_current_coordinates()
     time.sleep(1)
